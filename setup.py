1eb9f608a8b1b9c26a6a298c62adf8ea965406fc
suryasr007 <94suryateja@gmail.com>
2018-05-09 16:01:18 +0530

    Added license, manifest, setup files

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+VERSION = 'v1.0.0'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='random-password-generator',
+    version=VERSION,
+    description='Simple and custom random password generator for python',
+    long_description=long_description,
+    url='https://github.com/suryasr007/password-generator',
+    author='Surya Teja Reddy Valluri',
+    author_email='94suryateja@gmail.com',
+    license='MIT',
+    py_modules=['password_generator'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX'
+    ],
+    keywords='random password generator different lengths'
+)
