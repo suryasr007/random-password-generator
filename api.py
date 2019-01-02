ce9c98416f1fd2f6919f126d25f2f7259a448622
Arthur Coelho <arthur.cueio@gmail.com>
2019-01-02 20:45:31 -0200

    Frontend!
    api.py reformated using PyCharm
    
    Basic frontend done
    
    Fixed css link for flask
    
    Added footer
    
    Added error message.
    
    Added info message

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, render_template, request
 from flask_restful import Resource, Api, reqparse
 from webargs import fields, validate
 from webargs.flaskparser import use_args
 from password_generator import PasswordGenerator
+import re
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
@@ -46,6 +47,7 @@ non_duplicate_args = {
     'maxlen': fields.Int(required={'message': 'maximum length required', 'code': 400})
 }
 
+
 class PasswordGenerator(Resource):
 
     @use_args(password_generator_args)
@@ -55,8 +57,8 @@ class PasswordGenerator(Resource):
             res = pwg.generate()
         except Exception as e:
             abort(404, str(e))
-        
-        return jsonify({'password':res})
+
+        return jsonify({'password': res})
 
 
 class ShufflePassword(Resource):
@@ -67,8 +69,9 @@ class ShufflePassword(Resource):
             res = pwg.shuffle_password(password=args["password"], maxlen=args["maxlen"])
         except Exception as e:
             abort(404, str(e))
-        
-        return jsonify({'password':res})
+
+        return jsonify({'password': res})
+
 
 class NonDuplicatePassword(Resource):
 
@@ -78,17 +81,96 @@ class NonDuplicatePassword(Resource):
             res = pwg.non_duplicate_password(maxlen=args["maxlen"])
         except Exception as e:
             abort(404, str(e))
-        
-        return jsonify({'password':res})
 
-@app.route('/')
+        return jsonify({'password': res})
+
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return "for more info: <a href='https://github.com/suryasr007/random-password-generator/blob/master/README.md#api-get-request'> RPG docs</a>"
+
+    if request.method == "GET":
+        return render_template("home.html",
+                               minlen=6,
+                               maxlen=16,
+                               minuchars=1,
+                               minlchars=1,
+                               minnumbers=1,
+                               minschars=1
+                               )
+
+    # Check for generate = 1
+    if not int(request.form.get("generate")) == 1:
+        return "Something went wrong :( Try pressing the button again"
+
+    # Start if minlen is greater than maxlen
+    if int(request.form.get("minlen")) > int(request.form.get("maxlen")):
+        return render_template("home.html",
+                               minlen=request.form.get("minlen"),
+                               maxlen=request.form.get("maxlen"),
+                               minuchars=request.form.get("minuchars"),
+                               minlchars=request.form.get("minlchars"),
+                               minnumbers=request.form.get("minnumbers"),
+                               minschars=request.form.get("minschars"),
+                               excludechars=request.form.get("excludechars"),
+                               error="Minimum length cannot be greater than maximum length."
+                               )
+
+    # Check if min chars options summed are greater than maxlen
+    sumMinChars = int(request.form.get("minuchars")) + int(request.form.get("minlchars")) + int(request.form.get("minnumbers")) +int(request.form.get("minschars"))
+    infoMsg = ""
+    if sumMinChars > int(request.form.get("maxlen")):
+        # Display info message
+        infoMsg = "Minimum chars parameters exceed maximum length. Automatically set maximum length to " + str(sumMinChars)
+
+    # Check for chars to be excluded
+    if not request.form.get("excludechars") == "":
+        excludechars = request.form.get("excludechars")
+        lowertoexclude = ""
+        uppertoexclude = ""
+        numberstoexclude = ""
+        specialtoexclude = ""
+        for c in range(0, len(excludechars)):
+            if re.match("^[a-z]", excludechars[c]):
+                lowertoexclude += excludechars[c]
+            elif re.match("^[A-Z]", excludechars[c]):
+                uppertoexclude += excludechars[c]
+            elif re.match("^[0-9]", excludechars[c]):
+                numberstoexclude += excludechars[c]
+            elif not re.match("^[a-zA-Z0-9_]*$", excludechars[c]):
+                specialtoexclude += excludechars[c]
+
+        pwg.excludelchars = lowertoexclude
+        pwg.excludeuchars = uppertoexclude
+        pwg.excludenumbers = numberstoexclude
+        pwg.excludeschars = specialtoexclude
+
+    # Set pwg args
+    pwg.minlen = int(request.form.get("minlen"))
+    pwg.maxlen = int(request.form.get("maxlen"))
+    pwg.minuchars = int(request.form.get("minuchars"))
+    pwg.minlchars = int(request.form.get("minlchars"))
+    pwg.minnumbers = int(request.form.get("minnumbers"))
+    pwg.minschars = int(request.form.get("minschars"))
+
+    # Generate password
+    pwd = pwg.generate()
+
+    return render_template("home.html",
+                           pwd=pwd,
+                           minlen=request.form.get("minlen"),
+                           maxlen=request.form.get("maxlen"),
+                           minuchars=request.form.get("minuchars"),
+                           minlchars=request.form.get("minlchars"),
+                           minnumbers=request.form.get("minnumbers"),
+                           minschars=request.form.get("minschars"),
+                           excludechars=request.form.get("excludechars"),
+                           info=infoMsg
+                           )
+
 
 api.add_resource(PasswordGenerator, '/generate')
 api.add_resource(ShufflePassword, '/shuffle')
 api.add_resource(NonDuplicatePassword, '/nonduplicate')
-
 
 if __name__ == '__main__':
     app.run()
